ed9037ca92e063aa411d28fec799cba1d8859940
rajdeepnag <rajdeepnag12@gmail.com>
2021-06-27 19:58:14 +0530

    Multiple problems with this project

--- PracticeWebAppToDoList/src/main/java/webapp/LoginServlet.java
+++ PracticeWebAppToDoList/src/main/java/webapp/LoginServlet.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 
-@WebServlet(urlPatterns = "/login.do")
+@WebServlet(name=urlPatterns = "/login.do")
 public class LoginServlet extends HttpServlet{
 
 	protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException{
